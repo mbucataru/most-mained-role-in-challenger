e5416e449388400fb6bb08a68b1557c75121f92b
mbucataru <mbucatarugithub@gmail.com>
2023-09-26 12:37:16 -0400

    Refactor code and add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+<h3>A program that finds the most mained role in Challenger</h3>
+
+<p>This works by iterating through each challenger player, checking their last 7 matches for their role, then returning
+the most frequent one to our total counting array.</p>
+
+<p>Once every player has been iterated through, the program prints the most frequent role from all of the most frequent 
+roles of each player</p>
+
+Limitations:
+<ul>
+    <li>Quite slow. Has to check each match history of each player.</li>
+    <li>Only checks the first seven ranked matches. If a player has not played ranked
+    in seven games, they are not included in the tally.</li>
+</ul>
+
--- get_data/main.py
+++ get_data/main.py
@@ -1,12 +1,12 @@
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import LolWatcher
 from collections import defaultdict
+from pprint import pprint
 
-LAN = 'LA1'
+# REGION constant can be set to any desired region code
+REGION = 'NA1'
 
-NA = 'NA1'
-
-# REGION constant can be set to any of the above regions
-REGION = NA
+# QueueID value for Ranked
+RANKED_QUEUE = 420
 
 
 # Gets API key from a file in .gitignore, hiding the key.
@@ -18,91 +18,56 @@ def api_key():
 api = LolWatcher(api_key())
 
 
-# Returns an array of all challenger players on NA
-def challenger_players():
+def get_challenger_players_ids():
     challenger_league = api.league.challenger_by_queue(region=REGION, queue='RANKED_SOLO_5x5')
     return challenger_league['entries']
 
 
-# Takes in a summoner and returns its PUUID
-def puuid(summoner):
+def get_puuid_from_summoner(summoner):
     return api.summoner.by_id(region=REGION, encrypted_summoner_id=summoner['summonerId'])['puuid']
 
 
-# Takes in a PUUID and returns its matchlist (an array of match IDs)
-def matchlist(summoner_puuid):
+def get_matchlist_from_puuid(summoner_puuid):
     return api.match.matchlist_by_puuid(region=REGION, puuid=summoner_puuid)
 
 
-# Takes in a match and returns the names of the players in the game
-def names_of_participants(match):
-    for player in match['metadata']['participants']:
-        print(api.summoner.by_puuid(region=REGION, encrypted_puuid=player)['name'])
-
-dd = defaultdict(int)
 # Takes in either a puuid or a name and returns a dict of their role frequency in their last 20 games
-def most_played_role(puuid=None, name=None, counter_dict=None):
-    if name:
-        summoner = api.summoner.by_name(region=REGION, summoner_name=name)
-    elif puuid:
-        summoner = api.summoner.by_puuid(region=REGION, encrypted_puuid=puuid)
-    else:
-        raise ValueError("You must provide either a PUUID or a name.")
-    print(summoner)
-    summoner_matchlist = matchlist(summoner['puuid'])
-    if counter_dict:
-        role_frequency = counter_dict
-    else:
-        role_frequency = defaultdict(int)
+def iterate_most_played_role_from_puuid(summoner_puuid, previous_role_frequency):
+    summoner = api.summoner.by_puuid(region=REGION, encrypted_puuid=summoner_puuid)
+    summoner_matchlist = api.match.matchlist_by_puuid(region=REGION, puuid=summoner_puuid)
+    summoner_role_frequency = defaultdict(int)
 
-    for match in summoner_matchlist[:3]:
+    for match in summoner_matchlist[:7]:
         match_details = api.match.by_id(region=REGION, match_id=match)
-        print(match_details)
-        matchid = match_details['info']['queueId']
-        if match_details['info']['queueId'] != 420:
-            print('skipped')
-            continue
+        match_type = match_details['info']['queueId']
         match_info = match_details['info']
+
+        if match_type != RANKED_QUEUE:
+            continue
+
         for player in match_info['participants']:
             if summoner['puuid'] == player['puuid']:
                 if player['individualPosition'] != 'Invalid':
-                    # print('THE DICT IS')
-                    # print(role_frequency)
-                    role_frequency[player['individualPosition']] += 1
-    if role_frequency:
-        role = max(role_frequency, key=role_frequency.get)
-        return role
+                    summoner_role_frequency[player['individualPosition']] += 1
+
+    if len(summoner_role_frequency) != 0:
+        role = max(summoner_role_frequency, key=summoner_role_frequency.get)
+        previous_role_frequency[role] += 1
+        print('Summoner ' + summoner['name'] + "'s main role is " + role)
+    else:
+        print('Summoner ' + summoner['name'] + "'s has not played ranked in 6 games")
 
 
 def most_mained_role_in_challenger():
-    summoners = challenger_players()
+    summoners = get_challenger_players_ids()
     main_role_frequency = defaultdict(int)
+
     for summoner in summoners:
-        summoner_puuid = puuid(summoner)
-        main_role_frequency[most_played_role(puuid=summoner_puuid)] += 1
-        print(main_role_frequency)
+        summoner_puuid = get_puuid_from_summoner(summoner)
+        iterate_most_played_role_from_puuid(summoner_puuid, main_role_frequency)
+        pprint(dict(main_role_frequency))
+
     return max(main_role_frequency, key=main_role_frequency.get)
 
 
-# challenger_players = challenger_players()
-# print(challenger_players)
-
-# summoner1_matchlist = matchlist(puuid(challenger_players[0]))
-
-
-print(most_mained_role_in_challenger())
-
-# most_played_roles(puuid='tWaCuoPkUPuqJIPvDeGkSVp1MFsVaJ1ukNJjVSZVif3cBGo9KZPsvbWRga9Mmo6cZYjxUgXWbBIutw')
-
-# first_match = api.match.by_id(region=REGION, match_id=summoner1_matchlist[0])
-
-
-# print(first_match)
-
-# print(api.match.timeline_by_match(REGION, matchlist[0]))
-
-
-# names_of_participants(first_match)
-
-# for player in challenger_players['entries']:
-# print(player['summonerName'])
+print('The most mained role in challenger is ' + most_mained_role_in_challenger())
